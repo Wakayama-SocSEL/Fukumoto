995842284cf0ae582e8eccf9165d8243e34333fc
Fukumoto Haruki <s216239@center.wakayama-u.ac.jp>
2019-01-21 11:37:39 +0900

    テスト用javaファイルの追加

---
+++ evosuite_test/Main.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+
+        int subAnswer = calculate.subtraction(a, b);
+        int addAnswer = calculate.add(a, b);
+
+        System.out.println("5 + 2 = " + addAnswer);
+        System.out.println("5 - 2 = " + subAnswer);
+    }
+}
---
+++ evosuite_test/calculate.java
@@ -0,0 +1,13 @@
+public class calculate{
+    public static int add(int x, int y){
+        int answer = x + y;
+
+        return answer;
+    }
+
+    public static int subtraction(int x, int y){
+        int answer = x - y;
+
+        return answer;
+    }
+}
